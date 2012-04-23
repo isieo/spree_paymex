1ee00dc4ae8aaa1cb3f6d2ec163c90ce70ce910f
Calvin Tee <calvin@alphamelon.com>
2012-04-23 18:06:35 +0800

    pan is not merchant id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Add your extension routes here
   
-  match '/gateway/paymex/request'  => 'spree/paymex#response', :as => :paymex_response, :via=>[:post,:get]
+  match '/paymex/response'  => 'spree/paymex#response', :as => :paymex_response, :via=>[:post,:get]
+  match '/paymex/inquiry'  => 'spree/paymex#inquiry', :as => :paymex_inquiry, :via=>[:post,:get]
+  match '/paymex/notify'  => 'spree/paymex#notify', :as => :paymex_notify, :via=>[:post,:get]
+  match '/paymex/void'  => 'spree/paymex#void', :as => :paymex_void, :via=>[:post,:get]
 
 end
