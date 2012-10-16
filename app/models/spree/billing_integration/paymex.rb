0a365115daab60094b1108c3bc4a40c6e6155457
Calvin Tee <calvin@alphamelon.com>
2012-10-16 23:21:34 +0800

    add prepending zeros to mid

--- app/models/spree/billing_integration/paymex.rb
+++ app/models/spree/billing_integration/paymex.rb
@@ -6,13 +6,13 @@ class Spree::BillingIntegration::Paymex < Spree::BillingIntegration
   def payment_profiles_supported?
     false
   end
-  
+
   def merchant_id_with_checksum(order_id)
     sum = 0
     order_id.split(//).each do |c|
       sum += c.ord
     end
-    preferred_merchant_id.to_i + sum.to_i
+    "#{preferred_merchant_id.to_i + sum.to_i}".rjust(13,'0')
   end
 
 end
