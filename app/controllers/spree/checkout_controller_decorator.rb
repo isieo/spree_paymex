860d802f19cf1450928a363d38bdf103c29193cb
haziq <haziq@alphamelon.com>
2014-05-30 16:41:52 +0800

    Update checkout_controller_decorator.rb

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -7,7 +7,7 @@ CheckoutController.class_eval do
   def redirect_for_paymex
     return unless params[:state] == "payment"
     @payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-    if @payment_method && @payment_method.kind_of?(Spree::PaymentMethod::Paymex)
+    if @payment_method && @payment_method.kind_of?(Spree::BillingIntegration::Paymex)
       redirect_to paymex_proxy_path(:gateway_id => @payment_method.id, :order_id => @order.id)
     end
   end
