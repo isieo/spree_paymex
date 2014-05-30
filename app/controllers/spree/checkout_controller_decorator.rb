fbc89785aa5b97b287ab8bdaafa8eb3621122669
haziq <haziq@alphamelon.com>
2014-05-30 16:40:58 +0800

    Update checkout_controller_decorator.rb

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,4 +1,3 @@
-=begin
 CheckoutController.class_eval do
 
   before_filter :redirect_for_dotpay_pl, :only => :update
@@ -8,10 +7,9 @@ CheckoutController.class_eval do
   def redirect_for_paymex
     return unless params[:state] == "payment"
     @payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
-    if @payment_method && @payment_method.kind_of?(PaymentMethod::Paymex)
-      redirect_to gateway_paymex_path(:gateway_id => @payment_method.id, :order_id => @order.id)
+    if @payment_method && @payment_method.kind_of?(Spree::PaymentMethod::Paymex)
+      redirect_to paymex_proxy_path(:gateway_id => @payment_method.id, :order_id => @order.id)
     end
   end
 
 end
-=end
